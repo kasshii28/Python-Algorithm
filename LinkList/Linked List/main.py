c4475a99fb6b6e4874ab2cafdf9fa422a6fb7b6b
kasshii28 <gp22a043@oecu.jp>
2024-09-15 14:02:42 +0900

    add linkedlist doublylinkedlist

--- LinkList/Linked List/main.py
+++ LinkList/Linked List/main.py
@@ -9,8 +9,8 @@ class Node(object):
 
 class LinkedList(object):
   def __init__(self, head=None)-> None:
-    self.head = None
-
+    self.head = head
+  
   def append(self, data: Any)-> None:
     new_node = Node(data)
     if self.head is None:
@@ -49,14 +49,63 @@ class LinkedList(object):
     
     previous_node.next = current_node.next
     
+  def reverse_iterative(self) -> None:
+    previous_node = None
+    current_node = self.head
+    while current_node:
+      next_node = current_node.next
+      current_node.next = previous_node
+      previous_node = current_node
+      current_node = next_node
+
+    self.head = previous_node
+
+  def reverse_recursive(self) -> None:
+    def _reverse_recursive(current_node: Node, previous_node: Node) -> Node:
+      if not current_node:
+        return previous_node
+      
+      next_node = current_node.next
+      current_node.next = previous_node
+      previous_node = current_node
+      current_node = next_node
+      return _reverse_recursive(current_node, previous_node)
+
+    self.head = _reverse_recursive(self.head, None)
+
+  
+  def reverse_even(self) -> None:
+    def _reverse_even(head: Node, previous_node: Node):
+      if head is None:
+        return Node
+
+      current_node = head
+      while current_node and current_node.data % 2 == 0:
+        next_node = current_node.next
+        current_node.next = previous_node
+        previous_node = current_node
+        current_node = next_node
+      
+      if current_node != head:
+        head.next = current_node
+        _reverse_even(current_node, None)
+        return previous_node
+      else:
+        head.next = _reverse_even(head.next, head)
+        return head
+    
+    self.head = _reverse_even(self.head, None)
 
 if __name__ == '__main__':
    l = LinkedList()
+   l.append(0)
    l.append(1)
    l.append(2)
    l.append(3)
-   l.insert(0)
-   print(l.head.data)
-   print(l.head.next.data)
-   print(l.head.next.next.data)
-   print(l.head.next.next.next.data)
+   l.print()
+   print("#########")
+   l.reverse_iterative()
+   l.print()
+   print("#########")
+   l.reverse_recursive()
+   l.print()
